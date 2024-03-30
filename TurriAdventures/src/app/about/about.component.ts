2090c8145307a14d7848556582dca793003a958a
Titicc <83614737+Titicc@users.noreply.github.com>
2024-03-30 17:11:31 -0600

    about completo

--- TurriAdventures/src/app/about/about.component.ts
+++ TurriAdventures/src/app/about/about.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 @Component({
   selector: 'app-about',
   standalone: true,
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
-  imports: [CommonModule]
+  imports: [CommonModule,SidebarComponent,HeaderComponent,FooterComponent]
 
 })
 export class AboutComponent {
