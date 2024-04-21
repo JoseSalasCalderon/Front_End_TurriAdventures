1c8008c3170e4abc1e320a32a7294c436d48e9d5
Titicc <83614737+Titicc@users.noreply.github.com>
2024-04-21 13:59:12 -0600

    Conectar angular con api

---
+++ TurriAdventures/src/Core/ReservaService.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Reserva } from '../Model/Reserva';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationService {
+
+
+  private url: string = ""
+
+  constructor(private http:HttpClient) {
+    this.url = "https://localhost:7032/Reservacions/ListarReservas";
+   }
+
+
+   getList():Observable<Reserva[]>{
+    return this.http.get<Reserva[]>(this.url);
+}
+
+
+
+
+}
