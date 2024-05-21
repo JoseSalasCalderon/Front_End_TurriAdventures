d79c70b7bc97cc8be1b59937f7f850bd6c3b6457
najeramaria <najeramaria05@gmail.com>
2024-05-21 00:19:15 -0600

    Envió correo

--- TurriAdventures/src/app/reserve/reserveConfirm/confirmarreserva.component.ts
+++ TurriAdventures/src/app/reserve/reserveConfirm/confirmarreserva.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from "../../sidebar/sidebar.component";
 import { DatosCompartidosService } from '../DatosCompartidosService';
 import { ActivatedRoute } from '@angular/router';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-confirmarreserva',
@@ -14,20 +15,30 @@ export class ConfirmarreservaComponent implements OnInit {
   datosReserve: { fechaLlegada: string, fechaSalida: string, tipoHabitacion: number } = { fechaLlegada: '', fechaSalida: '', tipoHabitacion: 0 };
   datosCliente: { idCliente: string, nombre: string, apellidos: string, email: string } = { idCliente: '', nombre: '', apellidos: '', email: '' };
 
-  habitacionId: any;
-
-  idReserva: number = 0;
-
-
   constructor(private datosCompartidosService: DatosCompartidosService,
     private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.datosReserve = this.datosCompartidosService.getDatosReserve();
     this.datosCliente = this.datosCompartidosService.getDatosCliente();
-    // this.route.queryParams.subscribe(params => {
-    //   this.idReserva = params['idReserva']; // Obtiene el ID de la reserva desde los parámetros de la URL
-    // });
+    this.enviarCorreo();
   }
-  
+
+  enviarCorreo() {
+    const templateParams = {
+      nombre: this.datosCliente.nombre,
+      apellidos: this.datosCliente.apellidos,
+      email: this.datosCliente.email,
+      fechaLlegada: this.datosReserve.fechaLlegada,
+      fechaSalida: this.datosReserve.fechaSalida,
+      idCliente: this.datosCliente.idCliente
+    };
+
+    emailjs.send('service_xpb0hjs', 'template_1y2o4cu', templateParams, 'lnkmeUkx_FGf2yVZ1')
+      .then((result: EmailJSResponseStatus) => {
+        console.log('Email sent successfully!', result.status, result.text);
+      },
+      );
+  }
+
 }
