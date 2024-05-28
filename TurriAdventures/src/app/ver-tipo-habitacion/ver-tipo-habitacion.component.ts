1a43494d52022414c229f6bf5b7d773cfe578c98
JoseSalasCalderon <joseant2302@gmail.com>
2024-05-28 12:33:03 -0600

    Guardado en db y carga desde cloudinary funcional

--- TurriAdventures/src/app/ver-tipo-habitacion/ver-tipo-habitacion.component.ts
+++ TurriAdventures/src/app/ver-tipo-habitacion/ver-tipo-habitacion.component.ts
@@ -44,11 +44,10 @@ export class VerTipoHabitacionComponent implements OnInit{
       const tipoHabitacionInt = parseInt(tipoHabitacionSeleccionada);
       this.tiposHabitacionService.BuscarTipoHabitacionPorId(tipoHabitacionInt).subscribe((tipoHabitacion: TipoHabitacion) => {
         this.tipoHabitacionSeleccionada = tipoHabitacion;
-        const imageUrl = `assets/Habitaciones/${tipoHabitacion.imagenTipoHabitacion}`;
-        this.imageSrc = imageUrl;
+        this.imageSrc = tipoHabitacion.imagenTipoHabitacion;
         
         // Realiza una solicitud HTTP para obtener la imagen como un Blob
-        this.http.get(imageUrl, { responseType: 'blob' }).subscribe(blob => {
+        this.http.get(this.imageSrc, { responseType: 'blob' }).subscribe(blob => {
           this.selectedFile = new File([blob], tipoHabitacion.imagenTipoHabitacion, { type: blob.type });
           console.log(this.selectedFile);
         });
@@ -88,30 +87,26 @@ export class VerTipoHabitacionComponent implements OnInit{
 
   aceptar() {
     if (this.selectedFile) {
-      const formData = new FormData();
-      formData.append('file', this.selectedFile, this.selectedFile.name);
-      console.log(this.selectedFile.name);
-      // Agregar la ruta de la carpeta assets
-      const assetsPath = 'src/assets/Habitaciones'; // Ajusta esta ruta según la estructura de tu proyecto frontend
-      formData.append('assetsPath', assetsPath);
+     
       this.UploadImagesServiceService.subirImagen(this.selectedFile).subscribe((res) => {
         console.log(res);
-        // if (res) {
-        //   if (this.tipoHabitacionSeleccionada && this.selectedFile) {
-        //     this.tipoHabitacionSeleccionada.imagenTipoHabitacion = this.selectedFile.name;
-        //     this.tiposHabitacionService.ActualizarTipoHabitacion(this.tipoHabitacionSeleccionada).subscribe(response => {
-        //       // Abrir modal si la respuesta es true o false
-        //       if(response=true){
-        //         this.successMessage = 'Actualizó correctamente.';
-        //         this.errorMessage = null;
-        //       }else{
-        //         this.successMessage = null;
-        //         this.errorMessage = 'No se pudo actualizar.';
-        //       }
+        if (res) {
+          if (this.tipoHabitacionSeleccionada && this.selectedFile) {
+            this.tipoHabitacionSeleccionada.imagenTipoHabitacion = res.url;
+            this.tiposHabitacionService.ActualizarTipoHabitacion(this.tipoHabitacionSeleccionada).subscribe(response => {
+              // Abrir modal si la respuesta es true o false
+              if(response=true){
+                this.imageSrc = res.url;
+                this.successMessage = 'Actualizó correctamente.';
+                this.errorMessage = null;
+              }else{
+                this.successMessage = null;
+                this.errorMessage = 'No se pudo actualizar.';
+              }
   
-        //     });
-        //   }
-        // }
+            });
+          }
+        }
       });
       // this.http.post('https://localhost:7032/api/FileUpload/upload', formData).subscribe((response: any) => {
       //   console.log('File uploaded successfully', response);
