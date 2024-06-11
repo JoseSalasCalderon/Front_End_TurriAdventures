65f9843a8810f86f3ff5674ac419b2c0c0ed0e01
najeramaria <159678359+Leonarotti@users.noreply.github.com>
2024-06-11 01:39:13 -0600

    About sin paginacion

--- TurriAdventures/src/app/about-crud/about-crud.component.ts
+++ TurriAdventures/src/app/about-crud/about-crud.component.ts
@@ -1,32 +1,34 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { About } from '../../Model/About';
 import { AboutService } from '../../Core/AboutService';
-import { SidebarComponent } from '../sidebar/sidebar.component';
 import { UploadImagesServiceService } from '../../Core/upload-images-service.service';
-import { concatMap, map, toArray } from 'rxjs/operators';
-import { Observable, from } from 'rxjs';
+import { Router } from '@angular/router';
+import { SidebarAdministradorComponent } from '../sidebar-administrador/sidebar-administrador.component';
 
 @Component({
   selector: 'app-about-crud',
   standalone: true,
   templateUrl: './about-crud.component.html',
   styleUrls: ['./about-crud.component.css'],
-  imports: [CommonModule, SidebarComponent, HeaderComponent],
-  encapsulation: ViewEncapsulation.None,
+  imports: [CommonModule, SidebarAdministradorComponent],
 })
 export class AboutCrudComponent implements OnInit {
   descripcion: string = '';
   imagenes: string[] = [];
   imagenSeleccionada: string = '';
-  descripcionOriginal: string = '';
-  imagenesOriginales: string[] = [];
   about!: About[];
+  vistaPreviaImagen: string | ArrayBuffer | null = null;
+  
+  
+  mensaje: string = '';
+  esError: boolean = false;
 
   constructor(
     private aboutService: AboutService,
-    private uploadImagesService: UploadImagesServiceService
+    private uploadImagesService: UploadImagesServiceService,
+    private router: Router,
+
   ) { }
 
   ngOnInit(): void {
@@ -34,14 +36,12 @@ export class AboutCrudComponent implements OnInit {
   }
 
 
-  obtenerNosotros(): void {
+  async obtenerNosotros() {
     this.aboutService.ListarNosotros().subscribe((data: About[]) => {
       if (data.length > 0) {
         this.descripcion = data[0].descripcionNosotros;
-        this.imagenes = data.map(item => 'assets/Facilidades/' + item.imagenNosotros);
+        this.imagenes = data.map(item =>  item.imagenNosotros);
         this.imagenSeleccionada = this.imagenes[this.imagenes.length - 1];
-        this.descripcionOriginal = this.descripcion;
-        this.imagenesOriginales = [...this.imagenes];
         this.about = data;
       }
       console.log('about ts', this.about);
@@ -55,109 +55,88 @@ export class AboutCrudComponent implements OnInit {
 
   onArchivoSeleccionado(evento: any): void {
     if (evento.target.files && evento.target.files[0]) {
-      // Solo actualiza la imagen seleccionada, no la subas todavía
       const archivo = evento.target.files[0];
       const reader = new FileReader();
       reader.onload = (e: any) => {
-        this.imagenSeleccionada = e.target.result;
+        this.vistaPreviaImagen = e.target.result;
       };
       reader.readAsDataURL(archivo);
     }
   }
-
-
+  
   subirNuevaImagen(): void {
-    // Verifica que haya una imagen seleccionada
-    if (this.imagenSeleccionada) {
-      this.uploadImagesService.subirImagen(this.imagenSeleccionada).subscribe(
+    if (this.vistaPreviaImagen) {
+      this.uploadImagesService.subirImagen(this.vistaPreviaImagen).subscribe(
         (respuesta) => {
-          // Agrega la URL de la imagen subida a tu lista de imágenes
           this.imagenes.push(respuesta.secure_url);
           console.log('ruta imagen', respuesta.secure_url);
-
           const nuevoNosotros: About = {
-            idNosotros: 0, // Esto dependerá de cómo esté configurada tu API
+            idNosotros: 0,
             descripcionNosotros: this.descripcion,
             imagenNosotros: respuesta.secure_url
           };
           this.aboutService.CrearNosotros(nuevoNosotros).subscribe(response => {
-            // Abrir modal si la respuesta es true o false
-            if (response = true) {
-              console.log('Actualizó correctamente.');
+            if (response === true) {
+              this.mostrarMensaje('Imagen subida correctamente.', false);
+              this.obtenerNosotros();
             } else {
-              console.log('No se pudo actualizar.');
+              this.mostrarMensaje('No se pudo subir la imagen.', true);
             }
-
           });
-
-          // Limpia la imagen seleccionada después de subirla
+          this.vistaPreviaImagen = null;
           this.imagenSeleccionada = '';
-
         },
         (error) => {
           console.error('Error al subir la imagen:', error);
+          this.mostrarMensaje('Error al subir la imagen.', true);
+        },
+        () => {
+          this.obtenerNosotros(); // Llamada a obtenerNosotros() después de completar la subida de imagen
         }
       );
     }
   }
-  
+
+  confirmarEliminarImagen(idImagen: number): void {
+    if (confirm('¿Está seguro de que desea eliminar esta imagen?')) {
+      this.eliminarImagen(idImagen);
+    }
+  }
+
   eliminarImagen(idImagen: number): void {
     console.log('about ts a eliminar', this.about[idImagen].idNosotros);
     this.aboutService.EliminarNosotros(this.about[idImagen].idNosotros)
       .subscribe(() => {
-        console.log('Imagen eliminada con éxito');
-        this.obtenerNosotros(); // Actualiza la lista de imágenes después de eliminar
+        this.mostrarMensaje('La imagen se ha eliminado con éxito.', false);
+        this.obtenerNosotros(); 
       }, error => {
         console.error('Error al eliminar la imagen:', error);
+        this.mostrarMensaje('Error al eliminar la imagen.', true);
+
       });
   }
 
-  // subirImagenesSecuencialmente(): Observable<string[]> {
-  //   // Sube cada imagen secuencialmente y espera a que se complete antes de subir la siguiente
-  //   return from(this.imagenes).pipe(
-  //     concatMap((imagen) => this.uploadImagesService.subirImagen(imagen)),
-  //     map((respuesta) => respuesta.secure_url),
-  //     toArray()
-  //   );
-  // }
-
-  // guardarCambios(nuevaDescripcion: string): void {
-  //   // Asigna la nueva descripción
-  //   this.descripcion = nuevaDescripcion;
-
-  //   // Sube cada imagen de forma secuencial y luego guarda los cambios
-  //   this.subirImagenesSecuencialmente().subscribe(
-  //     (urlsImagenes) => {
-  //       // Crea una nueva entrada para "Nosotros" con la nueva descripción y las URLs de las imágenes subidas
-  //       const nuevoNosotros: About = {
-  //         idNosotros: 0, // Esto dependerá de cómo esté configurada tu API
-  //         descripcionNosotros: this.descripcion,
-  //         imagenNosotros: urlsImagenes// Concatena las URLs de las imágenes con comas
-  //       };
-
-  //       // Llama al servicio para crear la nueva entrada de "Nosotros"
-  //       this.aboutService.CrearNosotros(nuevoNosotros).subscribe(
-  //         () => {
-  //           alert('Cambios guardados');
-  //           // Vuelve a cargar los datos de "Nosotros" para reflejar los cambios en la interfaz
-  //           this.obtenerNosotros();
-  //         },
-  //         (error) => {
-  //           console.error('Error al guardar los cambios:', error);
-  //         }
-  //       );
-  //     },
-  //     (error) => {
-  //       console.error('Error al subir las imágenes:', error);
-  //     }
-  //   );
-  // }
-
   cancelarCambios(): void {
-    this.descripcion = this.descripcionOriginal;
-    this.imagenes = [...this.imagenesOriginales];
-    this.imagenSeleccionada = this.imagenes.length > 0 ? this.imagenes[this.imagenes.length - 1] : '';
+    this.router.navigate(['/modificarpaginas']);
   }
+  
+  actualizarDescripcion(newDescription: string){
+    const aboutToUpdate: About = {
+      idNosotros: this.about[0].idNosotros,  // Asegúrate de que estás usando el ID correcto
+      descripcionNosotros: newDescription,
+     imagenNosotros: this.about[0].imagenNosotros  // Asegúrate de que estás usando la imagen correcta
+   };
+
+   this.aboutService.EditarNosotros(aboutToUpdate).subscribe(
+    (response) => {
+      this.mostrarMensaje('La descripción se ha actualizado correctamente.', false);
+      this.obtenerNosotros();
+    },
+    (error) => {
+      this.mostrarMensaje('Error al actualizar la descripción.', true);
+    }
+  );
+}
 
   crearImagen() {
     const modal = document.getElementById('crearAnuncioModal');
@@ -170,66 +149,20 @@ export class AboutCrudComponent implements OnInit {
       const backdrop = document.createElement('div');
       backdrop.classList.add('modal-backdrop', 'fade', 'show');
       document.body.appendChild(backdrop);
+      this.obtenerNosotros();
+
     }
 
-    this.obtenerNosotros();
   }
-
-  // guardarImagen(event: Event) {
-  //   event.preventDefault();
-  //   if (this.imagenSeleccionada) {
-  //     this.uploadImagesService.subirImagen(this.imagenSeleccionada).subscribe(
-  //       (response) => {
-  //         const nuevaPublicidad: About = {
-  //           idNosotros: 0,
-  //           imagenNosotros: response.secure_url,
-  //           descripcionNosotros: ''
-  //         };
-
-  //         this.aboutService.CrearNosotros(nuevaPublicidad).subscribe(
-  //           (data) => {
-  //             this.obtenerNosotros();
-  //             this.cerrarModal();
-  //           }
-  //         );
-  //       },
-  //       (error) => {
-  //         this.mostrarMensaje('Error al subir la imagen', true);
-  //       }
-  //     );
-  //   }
-  // }
-
-  // onChangeImagen(event: Event) {
-  //   const input = event.target as HTMLInputElement;
-  //   if (input.files && input.files[0]) {
-  //     this.imagenSeleccionada = input.files[0];
-
-  //     const reader = new FileReader(); 
-
-  //     // reader.onload = (e: any) => {
-  //     //   const imagenActualElement = document.getElementById('imagenActual');
-  //     //   if (imagenActualElement) {
-  //     //     imagenActualElement.setAttribute('src', e.target.result);
-  //     //   }
-  //     // };
-
-  //     reader.onload = (e: any) => {
-  //       this.imagenActual = e.target.result;
-  //     };
-
-  //     reader.readAsDataURL(input.files[0]);
-  //   }
-  // }
-  mensaje: string = '';
-  esError: boolean = false;
 
   mostrarMensaje(mensaje: string, esError: boolean) {
     this.mensaje = mensaje;
     this.esError = esError;
+    this.obtenerNosotros();
     setTimeout(() => {
       this.mensaje = '';
     }, 3000);
+    this.obtenerNosotros();
   }
 
   cerrarModal() {
@@ -246,4 +179,24 @@ export class AboutCrudComponent implements OnInit {
       }
     }
   }
+
+  currentPage: number = 1;
+  pageSize: number = 8; 
+
+  imagenesPaginadas(): string[] {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    return this.imagenes.slice(startIndex, endIndex);
+  }
+  
+  totalPages(): number[] {
+    const total = Math.ceil(this.imagenes.length / this.pageSize);
+    return Array(total).fill(0).map((x, i) => i + 1);
+  }
+  
+  cambiarPagina(pageNumber: number): void {
+    this.currentPage = pageNumber;
+  }
+  
+
 }
--- TurriAdventures/src/app/about/about.component.ts
+++ TurriAdventures/src/app/about/about.component.ts
@@ -27,11 +27,11 @@ export class AboutComponent implements OnInit {
     this.obtenerNosotros();
   }
 
-  obtenerNosotros(): void {
+ async obtenerNosotros() {
     this.aboutService.ListarNosotros().subscribe((data: About[]) => {
       if (data.length > 0) {
         this.description = data[0].descripcionNosotros;
-        this.images = data.map(item => 'assets/Facilidades/' + item.imagenNosotros);
+        this.images = data.map(item => item.imagenNosotros);
         this.selectedImage = this.images[0];
       }
     });
@@ -39,5 +39,24 @@ export class AboutComponent implements OnInit {
 
   showImage(imageUrl: string): void {
     this.selectedImage = imageUrl;
+  }
+
+
+  currentPage: number = 1;
+  pageSize: number = 18; 
+
+  imagenesPaginadas(): string[] {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    return this.images.slice(startIndex, endIndex);
+  }
+  
+  totalPages(): number[] {
+    const total = Math.ceil(this.images.length / this.pageSize);
+    return Array(total).fill(0).map((x, i) => i + 1);
+  }
+  
+  cambiarPagina(pageNumber: number): void {
+    this.currentPage = pageNumber;
   }
 }
